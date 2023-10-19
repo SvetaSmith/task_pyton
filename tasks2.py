d75a40a527f429af559d31537b031a719e0a397b
SvetaSmith <148080482+SvetaSmith@users.noreply.github.com>
2023-10-19 11:15:16 +0200

    Update tasks2.py

--- tasks2.py
+++ tasks2.py
@@ -26,3 +26,48 @@ while run:
     break
 
 print("До свидания!")
+
+
+#task2
+
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
+
+
+tasks = []
+tod = []
+tom = []
+oth = []
+
+run = True
+
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(tasks)
+    print(tod)
+    print(tom)
+    print(oth)
+  elif command == "add":
+    task = input("Введите название задачи: ")
+    date = input("Введите дату: ")
+    if date == 'сегодня':
+      tod.append(task)
+    elif date == 'завтра':
+      tom.append(task)
+    else:
+      oth.append(task)
+    print("Задача добавлена")
+  elif command == "exit":
+    run = False
+    print('Спасибо за использование! До свидания!')
+  else: 
+    print("Неизвестная команда")
+    break
+
+print("До свидания!")
+
