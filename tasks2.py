badf044106056db286a0b3243b54d4fa1b78f3ca
SvetaSmith <148080482+SvetaSmith@users.noreply.github.com>
2023-10-19 10:50:49 +0200

    Update tasks2.py

--- tasks2.py
+++ tasks2.py
@@ -1,1 +1,28 @@
+#task1
+HELP = """
+help - напечатать справку по программе.
+add - добавить задачу в список (название задачи запрашиваем у пользователя).
+show - напечатать все добавленные задачи."""
 
+tasks = []
+
+run = True
+
+while run:
+  command = input("Введите команду: ")
+  if command == "help":
+    print(HELP)
+  elif command == "show":
+    print(tasks)
+  elif command == "add":
+    task = input("Введите название задачи: ")
+    tasks.append(task)
+    print("Задача добавлена")
+  elif command == "exit":
+    run = False
+    print('Спасибо за использование! До свидания!')
+  else: 
+    print("Неизвестная команда")
+    break
+
+print("До свидания!")
