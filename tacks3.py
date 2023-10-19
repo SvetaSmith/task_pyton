53e4ab122a68442dc2ba28fda95807469e095d4c
SvetaSmith <148080482+SvetaSmith@users.noreply.github.com>
2023-10-19 15:15:37 +0200

    Update tacks3.py

--- tacks3.py
+++ tacks3.py
@@ -1,1 +1,14 @@
 
+w = ['python', 'c++', 'c', 'scala', 'java']
+l = 'c'
+
+def count_letter(words,let):
+  count = 0
+  for word in words:
+    for letter in word:
+      if letter == let:
+        count += 1
+        break
+  return count
+
+print(count_letter(w,l))
