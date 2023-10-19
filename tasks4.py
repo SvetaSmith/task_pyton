20fea92eba6257e0ad09b737aefaa42c67df7ef5
SvetaSmith <148080482+SvetaSmith@users.noreply.github.com>
2023-10-19 15:33:12 +0200

    Update tasks4.py

--- tasks4.py
+++ tasks4.py
@@ -1,1 +1,19 @@
+token =""
+import telebot
 
+
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(content_types=["text"])
+def echo(message):
+    word = 'sveta'
+    string = message.text.lower()
+    if word in string:
+        bot.send_message(message.chat.id, 'Ба! Знакомые все лица!')
+    else:
+        bot.send_message(message.chat.id, message.text)
+
+
+bot.polling(none_stop=True)
